35c58745c6913eb699331e8c42606f73aac33946
WithLei <syunki@yeah.net>
2018-09-26 22:18:33 +0800

    9.26 listView update

--- app/src/main/java/com/android/renly/distancemeasure/Activity/ListActivity1.java
+++ app/src/main/java/com/android/renly/distancemeasure/Activity/ListActivity1.java
@@ -6,17 +6,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.view.animation.OvershootInterpolator;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +26,11 @@ import com.android.renly.distancemeasure.App;
 import com.android.renly.distancemeasure.Bean.MeasureData;
 import com.android.renly.distancemeasure.DB.MySQLiteOpenHelper;
 import com.android.renly.distancemeasure.R;
+import com.android.renly.distancemeasure.Utils.DimmenUtils;
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,32 +43,31 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator;
-import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
 
-public class ListActivity extends Activity {
+public class ListActivity1 extends Activity {
     @BindView(R.id.title)
     TextView title;
     @BindView(R.id.iv_toolbar_menu)
     ImageView ivToolbarMenu;
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
+    @BindView(R.id.listView)
+    SwipeMenuListView listView;
 
     private View dialogView;
     private EditText et_carId;
     private Spinner spinner;
     private EditText et_MACAddr;
-    private experimentAdapter adapter;
 
     private Unbinder unbinder;
 
-    private List<MeasureData>experimentList;
+    private List<MeasureData> experimentList;
     private static MySQLiteOpenHelper mySQLiteOpenHelper;
     private static SQLiteDatabase db;
+    private List<Map<String, Object>> list = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        setContentView(R.layout.activity_list1);
         unbinder = ButterKnife.bind(this);
         queryDB();
         initData();
@@ -74,19 +78,19 @@ public class ListActivity extends Activity {
         experimentList = new ArrayList<>();
         mySQLiteOpenHelper = MySQLiteOpenHelper.getInstance(this);
         db = mySQLiteOpenHelper.getWritableDatabase();
-        if (App.isFirstIn(this)){
+        if (App.isFirstIn(this)) {
             insertDB();
         }
         if (!db.isOpen())
             db = mySQLiteOpenHelper.getReadableDatabase();
         db.beginTransaction();
 
-        synchronized (mySQLiteOpenHelper){
+        synchronized (mySQLiteOpenHelper) {
             // 开启查询 获得游标
-            Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_NAME,null,null,null,null,null,null);
+            Cursor cursor = db.query(MySQLiteOpenHelper.TABLE_NAME, null, null, null, null, null, null);
 
             // 判断游标是否为空
-            if (cursor.moveToLast()){
+            if (cursor.moveToLast()) {
                 // 遍历游标
                 do {
                     String carid = cursor.getString(cursor.getColumnIndex(MySQLiteOpenHelper.carId));
@@ -98,9 +102,15 @@ public class ListActivity extends Activity {
                     int startDistance = cursor.getInt(cursor.getColumnIndex(MySQLiteOpenHelper.startDistance));
                     int nowDistance = cursor.getInt(cursor.getColumnIndex(MySQLiteOpenHelper.nowDistance));
 
-                    MeasureData data = new MeasureData(carid,carDirection,startDistance,nowDistance,result,measureTime,theTime,theID);
+                    Map<String, Object>items = new HashMap<String, Object>();
+                    items.put("carid",carid);
+                    items.put("measureTime",measureTime);
+                    items.put("result",result);
+                    items.put("time",theTime);
+                    list.add(items);
+                    MeasureData data = new MeasureData(carid, carDirection, startDistance, nowDistance, result, measureTime, theTime, theID);
                     experimentList.add(data);
-                }while (cursor.moveToPrevious());
+                } while (cursor.moveToPrevious());
             }
             cursor.close();
         }
@@ -132,7 +142,7 @@ public class ListActivity extends Activity {
 //            db.endTransaction();
 //        }
 
-        App.setFirst_In(this,false);
+        App.setFirst_In(this, false);
     }
 
     private String insertsql(MeasureData data) {
@@ -155,7 +165,7 @@ public class ListActivity extends Activity {
                 data.getTime() + "')";
     }
 
-    private void deleteDB(MeasureData data){
+    private void deleteDB(MeasureData data) {
         db = mySQLiteOpenHelper.getWritableDatabase();
         db.beginTransaction();
 
@@ -171,62 +181,101 @@ public class ListActivity extends Activity {
     }
 
     private void initView() {
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(DimmenUtils.dip2px(ListActivity1.this, 90));
+                // set item title
+                deleteItem.setTitle("删除");
+                // set item title fontsize
+                deleteItem.setTitleSize(18);
+                // set item title font color
+                deleteItem.setTitleColor(Color.WHITE);
+
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        listView.setMenuCreator(creator);
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                list.remove(position);
+                listView.invalidate();
+                return false;
+            }
+        });
+
+        // Left
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
+        listView.setAdapter(new SimpleAdapter(this,
+                list,
+                R.layout.item_experiment,
+                new String[]{"carid","measureTime","result","time"},
+                new int[]{R.id.carid,R.id.measureTime,R.id.result,R.id.time}));
         // 增加分割线
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        // Set RecyclerView ItemAnimator.
-        recyclerView.setItemAnimator(new SlideInLeftAnimator());
-
-//        SlideInUpAnimator animator = new SlideInUpAnimator(new OvershootInterpolator(1f));
-//        recyclerView.setItemAnimator(animator);
-
-        // 动画时长
-        recyclerView.getItemAnimator().setAddDuration(1000);
-        recyclerView.getItemAnimator().setRemoveDuration(1000);
-        recyclerView.getItemAnimator().setMoveDuration(1000);
-        recyclerView.getItemAnimator().setChangeDuration(1000);
-
-        // 插值器
-//        SlideInLeftAnimator animator = new SlideInLeftAnimator();
-//        animator.setInterpolator(new OvershootInterpolator());
-//        recyclerView.setItemAnimator(animator);
-
-        adapter = new experimentAdapter(this,experimentList);
-        recyclerView.setAdapter(new AlphaInAnimationAdapter(adapter));
-        adapter.setItemClickListener(new experimentAdapter.MyItemClickListener() {
-            @Override
-            public void onItemClick(View view, int pos) {
-                MeasureData obj = experimentList.get(pos);
-                Intent intent = new Intent(ListActivity.this, MainActivity.class);
-                intent.putExtra("isNewMeasure",false);
-                intent.putExtra("cardId",obj.getCarId());
-                intent.putExtra("direction",obj.getCarDirection());
-                intent.putExtra("startDirection",obj.getStartDistance());
-                intent.putExtra("measureTime",obj.getMeasureTime());
-                intent.putExtra("nowDirection",obj.getNowDistance());
-                intent.putExtra("result",obj.getResult());
-                intent.putExtra("theID",obj.getTheID());
-                startActivityForResult(intent, MainActivity.REQUEST_CODE);
-            }
-        });
-
-        adapter.setItemLongClickListener(new experimentAdapter.MyItemLongClickListener() {
-            @Override
-            public void onItemLongClick(View view, final int pos) {
-                new AlertDialog.Builder(ListActivity.this)
-                        .setTitle("请选择")
-                        .setItems(R.array.menu, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                deleteDB(experimentList.get(pos));
-                                adapter.remove(pos);
-                            }
-                        })
-                        .create()
-                        .show();
-            }
-        });
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+//        // Set RecyclerView ItemAnimator.
+//        recyclerView.setItemAnimator(new SlideInLeftAnimator());
+//
+////        SlideInUpAnimator animator = new SlideInUpAnimator(new OvershootInterpolator(1f));
+////        recyclerView.setItemAnimator(animator);
+//
+//        // 动画时长
+//        recyclerView.getItemAnimator().setAddDuration(1000);
+//        recyclerView.getItemAnimator().setRemoveDuration(1000);
+//        recyclerView.getItemAnimator().setMoveDuration(1000);
+//        recyclerView.getItemAnimator().setChangeDuration(1000);
+//
+//        // 插值器
+////        SlideInLeftAnimator animator = new SlideInLeftAnimator();
+////        animator.setInterpolator(new OvershootInterpolator());
+////        recyclerView.setItemAnimator(animator);
+//
+//        adapter = new experimentAdapter(this, experimentList);
+//        recyclerView.setAdapter(new AlphaInAnimationAdapter(adapter));
+//        adapter.setItemClickListener(new experimentAdapter.MyItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int pos) {
+//                MeasureData obj = experimentList.get(pos);
+//                Intent intent = new Intent(ListActivity1.this, MainActivity.class);
+//                intent.putExtra("isNewMeasure", false);
+//                intent.putExtra("cardId", obj.getCarId());
+//                intent.putExtra("direction", obj.getCarDirection());
+//                intent.putExtra("startDirection", obj.getStartDistance());
+//                intent.putExtra("measureTime", obj.getMeasureTime());
+//                intent.putExtra("nowDirection", obj.getNowDistance());
+//                intent.putExtra("result", obj.getResult());
+//                intent.putExtra("theID", obj.getTheID());
+//                startActivityForResult(intent, MainActivity.REQUEST_CODE);
+//            }
+//        });
+//
+//        adapter.setItemLongClickListener(new experimentAdapter.MyItemLongClickListener() {
+//            @Override
+//            public void onItemLongClick(View view, final int pos) {
+//                new AlertDialog.Builder(ListActivity1.this)
+//                        .setTitle("请选择")
+//                        .setItems(R.array.menu, new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                deleteDB(experimentList.get(pos));
+//                                adapter.remove(pos);
+//                            }
+//                        })
+//                        .create()
+//                        .show();
+//            }
+//        });
+//
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
 
 
@@ -243,13 +292,13 @@ public class ListActivity extends Activity {
                         String carId = et_carId.getText().toString();
                         String MACAddr = et_MACAddr.getText().toString();
                         if (TextUtils.isEmpty(carId))
-                            Toast.makeText(ListActivity.this, "请确认输入所有数据", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(ListActivity1.this, "请确认输入所有数据", Toast.LENGTH_SHORT).show();
                         else {
-                            Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                            Intent intent = new Intent(ListActivity1.this, MainActivity.class);
                             intent.putExtra("cardId", carId);
                             intent.putExtra("direction", spinner.getSelectedItem().toString());
-                            intent.putExtra("MACAddr",MACAddr);
-                            intent.putExtra("isNewMeasure",true);
+                            intent.putExtra("MACAddr", MACAddr);
+                            intent.putExtra("isNewMeasure", true);
                             startActivityForResult(intent, MainActivity.REQUEST_CODE);
                         }
                     }
@@ -292,7 +341,7 @@ public class ListActivity extends Activity {
     protected void onResume() {
         super.onResume();
         queryDB();
-        adapter.notifyDataSetChanged();
+//        adapter.notifyDataSetChanged();
     }
 
     @Override
